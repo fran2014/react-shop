d8ddd7203102ac76c2ab34ce837dada7f9916951
Francisco Teran <francisco011420@gmail.com>
2022-08-18 18:19:18 -0400

    [FEAT] react shop
    adding new order of routes to react shop

--- src/pages/SendEmail.jsx
+++ src/pages/SendEmail.jsx
@@ -1,32 +1,26 @@
 import React from "react";
-import "../styles/RecoveryPassword.scss";
+import "../styles/SendEmail.scss";
 
-const RecoveryPassword = () => {
+const SendEmail = () => {
   return (
-    <div className="login">
+    <div className="SendEmail">
       <div className="form-container">
         <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-
         <h1 className="title">Email has been sent!</h1>
         <p className="subtitle">
           Please check your inbox for instructions on how to reset the password
         </p>
-
         <div className="email-image">
           <img src="./icons/email.svg" alt="email" />
         </div>
-
-        <button className="primary-button login-button"> Login</button>
-
+        <button className="primary-button login-button">Login</button>
         <p className="resend">
-          <span>Don't receive the email?</span>
-          <a href="/" className="">
-            Resend
-          </a>
+          <span>Didn't receive the email?</span>
+          <a href="/">Resend</a>
         </p>
       </div>
     </div>
   );
 };
 
-export default RecoveryPassword;
+export default SendEmail;
